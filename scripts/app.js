d3c0a17f400c8fb115eaa75474a9c04db576a147
Henry Kaus <henrykaus19@gmail.com>
2024-03-10 21:32:37 -0700

    Enhanced accessibility of various elements and increased css selector uniformity

--- scripts/app.js
+++ scripts/app.js
@@ -6,17 +6,21 @@ let capstoneIsFlipped = false; // Capstone card is front-facing when page opens
  * On clicking the .nav__link, the menu is closed.
  */
 const handleNavButtonClick = (event) => {
-  if (navIsOpen) {
-    $('#dropdown-nav-btn')
-      .toggleClass('open')
-      .attr('aria-expanded', 'false');
-  } else {
-    $('#dropdown-nav-btn')
-      .toggleClass('open')
-      .attr('aria-expanded', 'true');
+  if ($('#dropdown-nav-btn').css('display') === 'none') {
+    return;
   }
 
   $('.nav__list').toggleClass('nav__list--shown');
+  $('#dropdown-nav-btn').toggleClass('nav__btn--open');
+
+  if (navIsOpen) {
+    $('#dropdown-nav-btn')
+      .attr('aria-expanded', 'false');
+  } else {
+    $('#dropdown-nav-btn')
+      .attr('aria-expanded', 'true');
+  }
+
   navIsOpen = !navIsOpen;
 }
 
@@ -26,14 +30,22 @@ const handleNavButtonClick = (event) => {
  */
 const handleFlipCapstoneCard = (event) => {
   if (capstoneIsFlipped) {
+    $('.capstone__card--front').attr('aria-hidden', 'false');
+    $('.capstone__card--back').attr('aria-hidden', 'true');
+
     $('.capstone__card--front').fadeIn(500);
     $('.capstone__card--back').fadeOut(500);
+    $('#flip-btn').attr('aria-label', 'Flip capstone card to back');
   } else {
+    $('.capstone__card--front').attr('aria-hidden', 'true');
+    $('.capstone__card--back').attr('aria-hidden', 'false');
+
     $('.capstone__card--front').fadeOut(500);
     $('.capstone__card--back')
       .css('display', 'flex')
       .hide()
       .fadeIn(500);
+    $('#flip-btn').attr('aria-label', 'Flip capstone card to front');
   }
 
   capstoneIsFlipped = !capstoneIsFlipped;
